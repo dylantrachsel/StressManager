57b05954edf447f52dd0f3cdac5c801385207bc7
dylantrachsel <dylan_trachsel4@hotmail.com>
2016-11-05 16:37:00 -0400

    Added classes to register views

--- Microsoft-Band-Heart-Rate-Demo-Android-master/app/src/main/java/com/example/jodrew/heartratebandapp/MainMenu.java
+++ Microsoft-Band-Heart-Rate-Demo-Android-master/app/src/main/java/com/example/jodrew/heartratebandapp/MainMenu.java
@@ -1,8 +1,20 @@
 package com.example.jodrew.heartratebandapp;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by dylantrachsel on 11/5/16.
  */
 
-public class MainMenu {
+public class MainMenu extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_menu,container, false);
+    }
 }
--- Microsoft-Band-Heart-Rate-Demo-Android-master/app/src/main/java/com/example/jodrew/heartratebandapp/PastBreakdown.java
+++ Microsoft-Band-Heart-Rate-Demo-Android-master/app/src/main/java/com/example/jodrew/heartratebandapp/PastBreakdown.java
@@ -1,8 +1,20 @@
 package com.example.jodrew.heartratebandapp;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by dylantrachsel on 11/5/16.
  */
 
-public class PastBreakdown {
+public class PastBreakdown extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.past_breakdown,container, false);
+    }
 }
